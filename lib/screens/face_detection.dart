fad273f311e41369f3b538d1fd7a5720c4e6ca33
Jesus Herrera <jesusherrera1594@gmail.com>
2025-09-13 10:57:48 -0600

    implement ai models

--- lib/screens/face_detection.dart
+++ lib/screens/face_detection.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import '../utils/camera_image.dart';
 
 class FaceDetection extends StatefulWidget{
   const FaceDetection({super.key});
@@ -8,11 +12,84 @@ class FaceDetection extends StatefulWidget{
 }
 
 class _FaceDetectionState extends State<FaceDetection> {
+  late List<CameraDescription> cameras;
+  late CameraController _cameraController;
+  Future<void> _initializeControllerFuture = Future.value();
+  final FaceDetector _faceDetector = FaceDetector(options: FaceDetectorOptions(
+    enableContours: false,
+    enableClassification: false,
+    enableLandmarks: false,
+    enableTracking: false,
+  ));
+
+  List<Face> _faces = [];
+  bool busy = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCameraController();
+  }
+
+  void _loadCameraController() async {
+    cameras = await availableCameras();
+    _cameraController = CameraController(cameras[0], ResolutionPreset.max, imageFormatGroup: Platform.isIOS ? ImageFormatGroup.bgra8888 : ImageFormatGroup.nv21);
+    _initializeControllerFuture = _cameraController.initialize().then((_) {
+      if (!mounted) return;
+      _cameraController.startImageStream(_processCameraImage);
+    });
+  }
+
+   _processCameraImage(CameraImage image) async {
+    if (busy) return;
+    final inputImage = inputImageFromCameraImage(image, cameras, 0, _cameraController);
+    if (inputImage == null) return;
+    busy = true;
+    final List<Face> faces = await _faceDetector.processImage(inputImage);
+    setState(() {
+      _faces = faces;
+      busy = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    _faceDetector.close();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Face detection"),
-    );
+    return FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                CameraPreview(_cameraController),
+                ..._faces.map((face) {
+                  final padding = MediaQuery.of(context).padding;
+                  return Positioned(
+                    left: face.boundingBox.right - face.boundingBox.width - padding.left,
+                    top: face.boundingBox.top - kToolbarHeight - padding.top,
+                    width: face.boundingBox.width,
+                    height: face.boundingBox.height,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.red, width: 2),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ],
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      );
   }
 }
--- lib/screens/text_recognition.dart
+++ lib/screens/text_recognition.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import '../utils/camera_image.dart';
 
 class TextRecognition extends StatefulWidget{
   const TextRecognition({super.key});
@@ -7,12 +11,93 @@ class TextRecognition extends StatefulWidget{
   State<TextRecognition> createState() => _TextRecognitionState();
 }
 
-class _TextRecognitionState extends State<TextRecognition> {
+class _TextRecognitionState extends State<TextRecognition>  {
+
+  late List<CameraDescription> cameras;
+  late CameraController _cameraController;
+  Future<void> _initializeControllerFuture = Future.value();
+   final TextRecognizer _textRecognizer =
+      TextRecognizer(script: TextRecognitionScript.latin);
+  String _recognizedText = '';
+  bool _isBusy = false;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCameraController();
+  }
+
+  void _loadCameraController() async {
+    cameras = await availableCameras();
+    _cameraController = CameraController(cameras[0], ResolutionPreset.max, imageFormatGroup: Platform.isIOS ? ImageFormatGroup.bgra8888 : ImageFormatGroup.nv21);
+    _initializeControllerFuture = _cameraController.initialize().then((_) {
+      if(!mounted) return;
+      _cameraController.startImageStream(_processCameraImage);
+    });
+  }
+
+  Future<void> _processCameraImage(CameraImage image) async {
+    if (_isBusy) return;
+    _isBusy = true;
+    final inputImage = inputImageFromCameraImage(image, cameras, 0, _cameraController);
+    if (inputImage == null) return;
+    try {
+
+      final RecognizedText recognisedText = await _textRecognizer.processImage(inputImage);
+      setState(() {
+        _isBusy = false;
+        _recognizedText = recognisedText.text;
+      });
+    } catch (e) {
+      print('Error occured during recognition: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    _textRecognizer.close();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Text Recognition"),
-    );
+    return Column(
+        children: <Widget>[
+          Expanded(
+            child: FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      CameraPreview(_cameraController),
+                      Positioned(
+                        bottom: 10,
+                        left: 10,
+                        right: 10,
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          color: Colors.black.withAlpha(100),
+                          child: Text(
+                            _recognizedText,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }
+                 else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+        ],
+      );
   }
 }
